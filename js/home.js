ab3e83867f74d55b124b89c84d7f23cbaa68a7ac
Salomon Smeke <ssmeke@luc.edu>
2017-04-02 00:00:55 -0500

    Making it less gross, oh god. I was so dumb. What will I think of this stuff 5 years down?

--- js/home.js
+++ js/home.js
@@ -1,40 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     startTime();
-    runMe();
-    if (window.attachEvent) {
-        window.attachEvent('onresize', function() {
-            runMe();
-        });
-    }
-    else if (window.addEventListener) {
-        window.addEventListener('resize', function() {
-            runMe();
-        }, true);
-    }
-    else {
-        //The browser does not support Javascript event binding
-    }
-
 });
-
-function runMe() {
-    var height = window.innerHeight;
-    var width = window.innerWidth;
-
-    document.getElementById('main-box').style.height = height+'px';
-    document.getElementById('main-box').style.width = width+'px';
-
-    document.getElementById('center-container').style.height = (height - document.getElementById('about-me').offsetHeight) + "px";
-    document.getElementById('top').style.height = (document.getElementById('center-container').offsetHeight/2 - document.getElementById('time').offsetHeight) + "px";
-}
-
 
 function startTime() {
     var today=new Date();
     var h=checkTime(today.getHours());
     var m=checkTime(today.getMinutes());
     var s=checkTime(today.getSeconds());
-    if (s%2==0){
+    if (s%2===0){
       document.getElementById('time').innerHTML = h + ':' + m + ':' + s;
     } else {
       document.getElementById('time').innerHTML = h + ':' + m + ' ' + s;
