eade07a96d9bc81ac7eff874668f20006700f51e
Salomon Smeke <ssmeke@luc.edu>
2017-06-24 23:50:24 -0500

    Codacy home.js

--- js/home.js
+++ js/home.js
@@ -1,32 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    startTime();
-});
-
 function startTime() {
     var today=new Date();
     var h=checkTime(today.getHours());
     var m=checkTime(today.getMinutes());
     var s=checkTime(today.getSeconds());
-    if (s%2===0){
-      document.getElementById('time').innerHTML = h + ':' + m + ':' + s;
-    } else {
-      document.getElementById('time').innerHTML = h + ':' + m + ' ' + s;
-    }
+    var timeString = s%2===0 ? h + ":" + m + ":" + s : h + ":" + m + " " + s;
+    document.getElementById("time").innerHTML = timeString;
     var twentyFour = 10.625;
     var sixty = 4.25;
     var c1 = Math.round(twentyFour*h).toString(16);
     var c2 = Math.round(sixty*m).toString(16);
     var c3 = Math.round(sixty*s).toString(16);
-    while (c1.length!=2){
-      c1 = "0"+c1;
-    }
-    while (c2.length!=2){
-      c2 = "0"+c2;
-    }
-    while (c3.length!=2){
-      c3 = "0"+c3;
-    }
-    document.getElementById('center-container').style.backgroundColor = "#" + c1 + c2 + c3;
+    [c1, c2, c3].forEach(function (c) {
+      c = c.length === 2 ? c : "0"+c;
+    });
+    document.getElementById("center-container").style.backgroundColor = "#" + c1 + c2 + c3;
     var t = setTimeout(function(){ startTime(); },500);
 }
 
@@ -34,3 +21,5 @@ function checkTime(i) {
     if (i<10) {i = "0" + i;}  // add zero in front of numbers < 10
     return i;
 }
+
+startTime();
