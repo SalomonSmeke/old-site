d08cf84a589a20686ed431e8c8134697ee3c004c
Salomon Smeke <ssmeke@luc.edu>
2017-06-24 23:56:16 -0500

    Fix bug in background color

--- js/home.js
+++ js/home.js
@@ -7,13 +7,15 @@ function startTime() {
     document.getElementById("time").innerHTML = timeString;
     var twentyFour = 10.625;
     var sixty = 4.25;
-    var c1 = Math.round(twentyFour*h).toString(16);
-    var c2 = Math.round(sixty*m).toString(16);
-    var c3 = Math.round(sixty*s).toString(16);
-    [c1, c2, c3].forEach(function (c) {
-      c = c.length === 2 ? c : "0"+c;
+    var backgroundColor = "#";
+    [
+      Math.round(twentyFour*h).toString(16),
+      Math.round(sixty*m).toString(16),
+      Math.round(sixty*s).toString(16)
+    ].forEach(function (c) {
+      backgroundColor += c.length === 2 ? c : "0" + c;
     });
-    document.getElementById("center-container").style.backgroundColor = "#" + c1 + c2 + c3;
+    document.getElementById("center-container").style.backgroundColor = backgroundColor;
     var t = setTimeout(function(){ startTime(); },500);
 }
 
